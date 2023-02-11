4a0f9d855e070bb31d6a0eacc0a3cfff7eb26272
hwijeen <hwijeen@gmail.com>
2023-02-11 14:30:53 -0500

    change save dir with lr

--- run_mask_cloze.sh
+++ run_mask_cloze.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export task=$1
-declare -A lr=(["mrpc"]=1e-5 ["rte"]=1e-4 ["cola"]=1e-5 ["sst2"]=1e-5 ["qnli"]=1e-4 ["qqp"]=1e-5 ["stsb"]=1e-5 ["mnli"]=2e-5 )
-declare -A metrics=(["mrpc"]=accuracy ["rte"]=accuracy ["cola"]=matthews_correlation ["sst2"]=accuracy ["qnli"]=accuracy ["qqp"]=accuracy ["stsb"]=pearson ["mnli"]=accuracy )
-declare -A eval_steps=(["mrpc"]=100 ["rte"]=100 ["cola"]=100 ["sst2"]=100 ["qnli"]=1000 ["qqp"]=1000 ["stsb"]=100 ["mnli"]=1000 )
+declare -A lr=(["mrpc"]=1e-5 ["rte"]=1e-4 ["cola"]=1e-5 ["sst2"]=2e-5 ["qnli"]=1e-5 ["qqp"]=1e-5 ["stsb"]=1e-5 ["mnli"]=2e-5)
+declare -A metrics=(["mrpc"]=accuracy ["rte"]=accuracy ["cola"]=matthews_correlation ["sst2"]=accuracy ["qnli"]=accuracy ["qqp"]=accuracy ["stsb"]=pearson ["mnli"]=accuracy)
+declare -A eval_steps=(["mrpc"]=100 ["rte"]=100 ["cola"]=100 ["sst2"]=100 ["qnli"]=1000 ["qqp"]=1000 ["stsb"]=100 ["mnli"]=1000)
 
-CUDA_VISIBLE_DEVICES=1 python run_glue.py \
+CUDA_VISIBLE_DEVICES=0 python run_glue.py \
   --model_name_or_path bert-base-cased \
   --task_name $task \
   --do_train \
@@ -13,10 +13,10 @@ CUDA_VISIBLE_DEVICES=1 python run_glue.py \
   --per_device_train_batch_size 32 \
   --learning_rate ${lr[$task]} \
   --num_train_epochs 10 \
-  --output_dir outs/mask_cloze/$task \
+  --output_dir outs/mask_cloze/${task}/${lr[$task]} \
   --logging_steps 10 \
   --report_to tensorboard \
-  --logging_dir logs/mask_cloze/$task \
+  --logging_dir logs/mask_cloze/${task}/${lr[$task]} \
   --evaluation_strategy steps \
   --eval_steps ${eval_steps[$task]} \
   --save_strategy steps \
