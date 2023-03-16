a0914cd90268e268b95de82cacc73a081f164568
hwijeen <hwijeen@gmail.com>
2023-03-16 01:27:00 -0400

    misc script formatting

--- scripts/launch_fisher.sh
+++ scripts/launch_fisher.sh
@@ -6,7 +6,8 @@
 #SBATCH --err=/projects/tir6/strubell/hahn2/mask/tir_logs/%x-%J-%a.err
 #SBATCH --time=96:00:00
 #SBATCH --mem=16G
-#SBATCH --gres=gpu:2080Ti:1 #SBATCH --exclude=tir-0-[7,9,13,15,17,19],tir-0-32
+#SBATCH --gres=gpu:2080Ti:1
+#SBATCH --exclude=tir-0-[7,9,13,15,17,19],tir-0-32
 
 source ~/anaconda3/etc/profile.d/conda.sh
 
