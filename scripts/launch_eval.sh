76477c2a4672419d84c1a20d2e883321b206b4ec
hwijeen <hwijeen@gmail.com>
2023-03-16 21:04:03 -0400

    add eval script

---
+++ scripts/launch_eval.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+#SBATCH --job-name=fisher
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hahn2@cs.cmu.edu
+#SBATCH --output=/projects/tir6/strubell/hahn2/mask/tir_logs/%x-%J-%a.out
+#SBATCH --err=/projects/tir6/strubell/hahn2/mask/tir_logs/%x-%J-%a.err
+#SBATCH --time=96:00:00
+#SBATCH --mem=16G
+#SBATCH --gres=gpu:2080Ti:1
+#SBATCH --exclude=tir-0-[7,9,13,15,17,19],tir-0-32
+
+source ~/anaconda3/etc/profile.d/conda.sh
+
+conda activate mask
+
+export HF_HOME=/scratch/hahn2/.cache
+
+cd /projects/tir6/strubell/hahn2/mask
+
+./scripts/run_eval.sh $1
--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
 export task=$1
-declare -A lr=(["mrpc"]=5e-4 ["rte"]=1e-4 ["cola"]=5e-4 ["sst2"]=5e-4 ["qnli"]=5e-4 ["qqp"]=5e-4 ["stsb"]=1e-5 ["mnli"]=5e-4)
-declare -A metrics=(["mrpc"]=accuracy ["rte"]=accuracy ["cola"]=matthews_correlation ["sst2"]=accuracy ["qnli"]=accuracy ["qqp"]=accuracy ["stsb"]=pearson ["mnli"]=accuracy)
-declare -A eval_steps=(["mrpc"]=100 ["rte"]=100 ["cola"]=100 ["sst2"]=100 ["qnli"]=1000 ["qqp"]=1000 ["stsb"]=100 ["mnli"]=1000)
+declare -A ft_lr=(["mrpc"]=3e-5 ["rte"]=3e-5 ["cola"]=1e-5 ["sst2"]=2e-5 ["qnli"]=2e-5 ["qqp"]=1e-5 ["stsb"]=1e-5 ["mnli"]=3e-5)
+declare -A mask_lr=(["mrpc"]=5e-4 ["rte"]=5e-4 ["cola"]=5e-4 ["sst2"]=5e-4 ["qnli"]=5e-4 ["qqp"]=5e-4 ["stsb"]=5e-4 ["mnli"]=5e-4)
 
-export model_path=/usr0/home/hahn2/mask/expose/outs/finetune/gluex/sst2/2e-5; export sparsity=0.00
-python run_glue.py \
-  --model_name_or_path $model_path \
-  --dataset_name $task \
-  --do_eval \
-  --output_dir eval_result \
-  --max_seq_length 128 \
-  --initial_sparsity $sparsity \
+
+declare -A eval_tasks=(\
+    ["sst2"]="imdb yelp_polarity amazon_polarity" \
+    # ["mnli"]="SNLI SICK"\
+    # ["qnli"]="NewsQA"\
+    ["RTE"]="mrpc qqp hans"\
+    ["mrpc"]="qqp rte hans"\
+    ["qqp"]="mrpc rte hans"\
+    # ["cola"]="grammar_test"\
+)
+
+# # finetune
+for eval_t in ${eval_tasks[$task]}
+do
+    export model_path=/projects/tir6/strubell/hahn2/mask/outs/finetune/${task}/${ft_lr[$task]}
+    echo $model_path $eval_t
+    python run_glue.py \
+      --model_name_or_path $model_path \
+      --dataset_name $eval_t \
+      --do_eval \
+      --output_dir eval_result/$task/$eval_t \
+      --max_seq_length 128 \
+      --initial_sparsity 0.0
+done
+
+# # mask
+# for eval_t in ${eval_tasks[$task]}
+# do
+#     export model_path=/projects/tir6/strubell/hahn2/mask/outs/mask_cloze/${task}/${mask_lr[$task]}
+#     echo $model_path $eval_t
+#     python run_glue.py \
+#       --model_name_or_path $model_path \
+#       --dataset_name $eval_t \
+#       --do_eval \
+#       --output_dir eval_result \
+#       --max_seq_length 128 \
+#       --initial_sparsity 0.0 \
+# done
