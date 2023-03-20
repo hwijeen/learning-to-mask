953a908108d97e6deb9a4e26ecf5ee8cd9560193
hwijeen <hwijeen@gmail.com>
2023-03-20 16:37:00 -0400

    add script for mask (not cloze)

--- scripts/run_mask.sh
+++ scripts/run_mask.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 export task=$1
-declare -A bz=( ["mrpc"]=32 ["rte"]=32 ["cola"]=32 ["sst2"]=128 ["qnli"]=128 )
-declare -A lr=(["mrpc"]=1e-5 ["rte"]=1e-5 ["cola"]=1e-5 ["sst2"]=1e-5 ["qnli"]=7e-4 )
+declare -A lr=(["mrpc"]=5e-4 ["rte"]=5e-4 ["cola"]=5e-4 ["sst2"]=5e-4 ["qnli"]=5e-4 ["qqp"]=5e-4 ["stsb"]=1e-5 ["mnli"]=5e-4)
+declare -A metrics=(["mrpc"]=accuracy ["rte"]=accuracy ["cola"]=matthews_correlation ["sst2"]=accuracy ["qnli"]=accuracy ["qqp"]=accuracy ["stsb"]=pearson ["mnli"]=accuracy)
+declare -A eval_steps=(["mrpc"]=100 ["rte"]=100 ["cola"]=100 ["sst2"]=100 ["qnli"]=1000 ["qqp"]=1000 ["stsb"]=100 ["mnli"]=1000)
 
-CUDA_VISIBLE_DEVICES=0 python run_glue.py \
+export sparsity=$2
+python run_glue.py \
   --model_name_or_path bert-base-cased \
-  --task_name $task \
+  --dataset_name $task \
   --do_train \
   --do_eval \
   --max_seq_length 128 \
-  --per_device_train_batch_size ${bz[$task]} \
+  --per_device_train_batch_size 32 \
   --learning_rate ${lr[$task]} \
   --num_train_epochs 10 \
-  --output_dir outs/$task \
+  --output_dir outs/mask/${task}/${lr[$task]}/${sparsity} \
   --logging_steps 10 \
   --report_to tensorboard \
-  --logging_dir logs/$task \
+  --logging_dir logs/mask/${task}/${lr[$task]}/${sparsity} \
   --evaluation_strategy steps \
-  --eval_steps 100 \
+  --eval_steps ${eval_steps[$task]} \
   --save_strategy steps \
   --save_total_limit 1 \
   --load_best_model_at_end \
-  --metric_for_best_model accuracy \
+  --metric_for_best_model ${metrics[$task]} \
   --overwrite_output_dir \
-  --initial_sparsity 0.05
+  --initial_sparsity $sparsity
